fdc9e2becd72a769a337ddd4b578c6cc50fb2225
AlanPatrik-iLAB <alan.santos@ilabqa.com>
2022-05-27 22:13:44 -0300

    first commit

---
+++ src/main/java/com/alanpatrik/createproducts/CreateproductsApplication.java
@@ -0,0 +1,13 @@
+package com.alanpatrik.createproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateproductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alanpatrik/createproducts/CreateproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanpatrik.createproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
